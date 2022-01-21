d30f9ee3ac37a32a782d3d31f56c6cc9214a739e
Vlad <makrak.vladislav@yandex.ru>
2022-01-21 19:48:01 +0300

    refactor: uncomment function

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,18 +1,18 @@
-//const body = document.body;
+const body = document.body;
 const hamburger = document.querySelector('.header__hamburger');
 const menuWrapper = document.querySelector('.header');
 const nav = document.querySelector('.nav__list');
 
 function toggleMenu() {
   menuWrapper.classList.toggle('open');
-  //body.classList.toggle('menu-open');
+  body.classList.toggle('menu-open');
 }
 hamburger.addEventListener('click', toggleMenu);
 
 function closeMenu(event) {
   if (event.target.classList.contains('nav__link')) {
     menuWrapper.classList.remove('open');
-    //body.classList.remove('menu-open');
+    body.classList.remove('menu-open');
   }
 }
 nav.addEventListener('click', closeMenu);
