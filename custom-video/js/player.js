3d5551bcd8c3dce005431dbafbe75047e8ff6e09
Vlad <makrak.vladislav@yandex.ru>
2022-02-03 03:02:43 +0300

    feat: add function toggleFullscreen

--- custom-video/js/player.js
+++ custom-video/js/player.js
@@ -1,23 +1,24 @@
 const player = document.querySelector('.video-player');
 const video = player.querySelector('.video-player__video');
-const progress = player.querySelector('.progress');
+const videoPoster = document.querySelector('.poster');
+const videoPosterButton = document.querySelector('.player__play');
 const progressBar = player.querySelector('.progress-bar');
 const toggle = player.querySelector('.player__button');
 const volumeSlider = player.querySelector('.player__volume');
 const volume = player.querySelector('.player__button-volume');
+const fullscreenButton = player.querySelector('.player__button-fullscreen');
 
 let videoVolume = volumeSlider.value / 100;
-
-console.log(videoVolume);
 
 function toggleBtn() {
   if (video.paused) {
     video.play();
-    console.log(videoVolume)
     video.volume = videoVolume;
+    videoPosterButton.classList.add('hide');
   } else {
     video.pause();
     video.volume = videoVolume;
+    videoPosterButton.classList.remove('hide');
   }
   toggle.classList.toggle('pause');
 }
@@ -34,6 +35,21 @@ function toggleMute() {
   volume.classList.toggle('mute');
 }
 
+function toggleFullscreen(event) {
+  if (document.fullscreenElement !== player) {
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+      fullscreenButton.classList.add('fullscreen-exit');
+    } else {
+      player.webkitRequestFullscreen?.();
+      fullscreenButton.classList.remove('fullscreen-exit');
+    }
+  } else {
+    document.exitFullscreen?.();
+    fullscreenButton.classList.remove('fullscreen-exit');
+  }
+}
+
 function handleVolume() {
   if (this.value == 0) {
     volume.classList.add('mute');
@@ -43,12 +59,13 @@ function handleVolume() {
     video.muted = false;
   }
   video[this.name] = this.value / 100;
+  volumeSlider.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${this.value}%, rgb(200, 200, 200) ${this.value}%, rgb(200, 200, 200) 100%)`;
 }
 
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.value = percent;
-  progressBar.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${percent}%, #fff ${percent}%, white 100%)`
+  progressBar.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${percent}%, rgb(200, 200, 200) ${percent}%, rgb(200, 200, 200) 100%)`;
 }
 
 function scrubProgress(event) {
@@ -57,24 +74,40 @@ function scrubProgress(event) {
 }
 
 toggle.addEventListener('click', toggleBtn);
+videoPoster.addEventListener('click', (event) => { 
+  event.target.classList.add('hide');
+  videoPosterButton.classList.add('hide');
+  toggleBtn();
+});
+videoPosterButton.addEventListener('click', () => {
+  videoPoster.classList.add('hide');
+  toggleBtn();
+});
 video.addEventListener('click', toggleBtn);
 video.addEventListener('timeupdate', handleProgress);
 volume.addEventListener('click', toggleMute);
 volumeSlider.addEventListener('change', handleVolume);
 volumeSlider.addEventListener('input', handleVolume);
+fullscreenButton.addEventListener('click', toggleFullscreen);
+document.addEventListener('fullscreenchange', (event) => {
+  fullscreenButton.classList.toggle('fullscreen-exit', document.fullscreenElement != null);
+});
 
 let drag;
 let grap;
-progressBar.addEventListener('click', scrubProgress);
-progressBar.addEventListener('mouseover', () => { drag = true });
-progressBar.addEventListener('mouseout', () => { drag = false; grap = false });
-progressBar.addEventListener('mousedown', () => { grap = drag });
-progressBar.addEventListener('mouseup', () => { grap = false });
-progressBar.addEventListener('mousemove', (event) => { 
+progressBar.addEventListener('pointerdown', scrubProgress);
+progressBar.addEventListener('pointerover', () => { drag = true });
+progressBar.addEventListener('pointerout', () => { drag = false; grap = false });
+progressBar.addEventListener('pointerdown', () => { grap = drag });
+progressBar.addEventListener('pointerup', () => { grap = false });
+
+progressBar.addEventListener('pointermove', (event) => { 
   if (drag && grap) {
     scrubProgress(event);
   }
 });
+
+
 
 
 
