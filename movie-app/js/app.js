68e533571e8f97c4ec544672d1d27c02ea353639
Vlad <makrak.vladislav@yandex.ru>
2022-02-10 23:52:37 +0300

    feat: add showPoster function

--- movie-app/js/app.js
+++ movie-app/js/app.js
@@ -2,138 +2,139 @@ const apiKey = 'api_key=1939abe3d00976407f86acd63c341f94';
 const baseUrl = 'https://api.themoviedb.org/3';
 const url = baseUrl + '/discover/movie?sort_by=popularity.desc&'+apiKey;
 const searchUrl = baseUrl + '/search/movie?'+apiKey;
-//http://api.themoviedb.org/3/movie/157336/videos?api_key=1939abe3d00976407f86acd63c341f94
-//search/movie?query=matrix&api_key=1939abe3d00976407f86acd63c341f94&append_to_response=videos
-//search/movie?query=matrix&api_key=1939abe3d00976407f86acd63c341f94
-//'&language=ru'
+
 const form = document.getElementById('search-form');
 const search = document.getElementById('search');
+const clearFormBtn =  document.querySelector('.form__clear');
+const moviePage = document.querySelector('.overlay-content');
 
 getData(url);
 
 async function getData(url) {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
   showData(data.results)
 }
 
-function showData(data) {
+getPosterData();
 
-  //const video = data.videos.results[0].key;
-  //console.log(video);
-
-  const moviesList = document.querySelector('.movies__list');
+const posterContainer = document.querySelector('.poster__wrapper');
+async function getPosterData() {
+  const res = await fetch(baseUrl + '/discover/movie/?'+apiKey+'&with_genres=28'+'&append_to_response=videos&region=US');
+  const data = await res.json();
+  posterContainer.innerHTML = '';
   console.log(data);
+  showPoster(data);
+}
+
+function dateConverter(data) {
+  return data.split('-').reverse().join('.')
+}
+
+function showPoster(data) {
+  console.log(data.results[0]);
+  let idMovie = Math.floor(Math.random() * (20 - 0 + 1) + 0);
+  let dataPoster = data.results[idMovie];
+  const posterTitle = dataPoster.title;
+  const poster = dataPoster.backdrop_path === null ? 'https://via.placeholder.com/420?text=No+photo' : `https://image.tmdb.org/t/p/w1280/${dataPoster.backdrop_path}`;
+  posterContainer.style.backgroundImage = `url('${poster}')`;
+  const movieDate = dateConverter(dataPoster.release_date);
+  const posterTemplate = `
+    <h2 class="poster__title">${posterTitle}</h2>
+    <div class="poster__info">
+      <span>Raiting: ${dataPoster.vote_average}</span>
+      <span>Votes: ${dataPoster.vote_count}</span>
+      <span>Release date: ${movieDate}</span>
+    </div>
+    <p class="poster__overview">${dataPoster.overview}</p>
+  `
+  posterContainer.insertAdjacentHTML('beforeend', posterTemplate);
+}
+
+function showData(data) {
+  console.log(data);
+  const moviesList = document.querySelector('.movies__list');
   moviesList.innerHTML = '';
+
+  if (data.length == 0) {
+    console.log('No results found');
+    moviesList.insertAdjacentHTML('beforeend', `<h3>No results found</h3>`);
+  }
+  
   data.forEach((element) => {
-    let img = '';
-    if (element.poster_path === null) {
-      img = 'https://via.placeholder.com/420?text=No+photo';
-    } else {
-      img = `https://image.tmdb.org/t/p/w500/${element.poster_path}`;
-    }
-    const title = element.title;
-    const rating = element.vote_average;
-    const overview = element.overview;
-    const id = element.id;
-    let ratingColor = '';
-    if (rating >= 7.5) {
+
+    const moviePoster = element.poster_path === null ? 'https://via.placeholder.com/420?text=No+photo' : `https://image.tmdb.org/t/p/w500/${element.poster_path}`;
+    const movieTitle = element.title;
+    const movieRating = element.vote_average;
+    const movieOverview = element.overview;
+    const movieId = element.id;
+
+    let ratingColor;
+    if (movieRating >= 7.5) {
       ratingColor = 'green';
-    } else if (rating < 7.5 && rating > 4.5) {
+    } else if (movieRating < 7.5 && movieRating > 4.5) {
       ratingColor = 'orange';
     } else {
       ratingColor = 'red';
     }
-    //const video = element.videos.results[0].key;
-    //console.log(element[index].videos.results[0].key);
-    //let dynamicHeight = Math.ceil(Math.random() * 30) + 100;
-    const cardTemplate = `<div class="movie card">
-    <img class="card__img" src="${img}" alt="${title}">
+
+    const movieCardTemplate = `<div class="movie card">
+    <img class="card__img" src="${moviePoster}" alt="${movieTitle}">
     <div class="card__info-wrapper">
       <div class="card__info">
-        <h3 class="card__title">${title}</h3>
-        <span class="card__rating ${ratingColor}">${rating}</span>
+        <h3 class="card__title">${movieTitle}</h3>
+        <span class="card__rating ${ratingColor}">${movieRating}</span>
       </div>
-      <button id="${id}" class="btn card__more-info">More info</button>
+      <button id="${movieId}" class="btn card__more-info">More info</button>
     </div>  
     <div class="card__overview">
     <h3>The overview</h3>
-    ${overview}
+    ${movieOverview}
     </div>
     </div>`
 
-    moviesList.insertAdjacentHTML('beforeend', cardTemplate);
-    document.getElementById(id).addEventListener('click', () => {
-      showPopup(element);
+    moviesList.insertAdjacentHTML('beforeend', movieCardTemplate);
+    document.getElementById(movieId).addEventListener('click', () => {
+      showMoviePage(element);
     });
   });
 }
-const moviePage = document.querySelector('.overlay-content');
 
-function showPopup(element) {
+
+function showMoviePage(element) {
   let id = element.id;
-  //https://api.themoviedb.org/3/movie/524434/?api_key=1939abe3d00976407f86acd63c341f94&append_to_response=videos
-  //http://api.themoviedb.org/3/movie/157336?api_key=###&append_to_response=videos
-  //+'&append_to_response=videos'
-  //credits
   fetch(baseUrl + '/movie/'+id+'?'+apiKey+'&append_to_response=videos').then(res => res.json()).then(data => {
     console.log(data);
     fetch(baseUrl + '/movie/'+id+'/credits?'+apiKey+'&append_to_response=videos').then(res => res.json()).then(credits => {
-      console.log(element.title);
-      console.log(credits);
-      //console.log(data);
-      
-      const movieDate = new Date(`${data.release_date}`);
-      //let trailerId = '';
-      let trailer = '';
       moviePage.innerHTML = '';
-      
+      const movieDate = new Date(`${data.release_date}`);
+
+      let movieTrailer = '';
       if (data.videos.results.length > 0) {
         trailerId = data.videos.results[0].key;
-        trailer = `<div class="movie-trailer">
-          <iframe src="https://www.youtube.com/embed/${data.videos.results[0].key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        movieTrailer = `<div class="movie-trailer">
+          <iframe src="https://www.youtube.com/embed/${trailerId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>`;
       } 
 
-      //let arrGenres = [];
-      /*
-      data.genres.forEach((element) => {
-        movieGenres.push(element.name);
-      });
-      */
-
       let movieGenres = data.genres.map((element) => {
         console.log(element.name);
-        return `<span>${element.name}</span>`
+        return `<button class="genres-btn btn" id="${element.id}">${element.name}</button>`
       }).join('');
 
-      console.log(movieGenres);
+      let movieCountries = data.production_countries.map((element) => {
+        return element.name;
+      }).join(''); 
 
-      let movieCountries = [];
-      data.production_countries.forEach((element) => {
-        movieCountries.push(element.name);
-      });
-
-      let homePage = '';
+      let movieHomePage; 
       if (data.homepage === null || data.homepage.length == 0) {
-        homePage = '';
+        movieHomePage = '';
       } else {
-        homePage = `<a href="${data.homepage}" class="btn" target="_blank">Learn to more</a>`;
+        movieHomePage = `<a href="${data.homepage}" class="btn" target="_blank">Learn to more</a>`;
       }
-      //movieGenres = movieGenres.join(', ');
-      movieCountries = movieCountries.join(', ');
 
-      /*
-      let movieCast = [];
-      credits.cast.forEach((element) => {
-        movieCast.push(element.name);
-      });
-      */
-      let arrCast = credits.cast.slice(0, 10);
-      let movieCast = arrCast.map((element) => {
-        //console.log(element.name);
-        let img = '';
+      let movieCast = credits.cast.map((element) => {
+        let img;
         if (element.profile_path === null) {
           img = 'https://via.placeholder.com/150?text=No+photo';
         } else {
@@ -146,12 +147,9 @@ function showPopup(element) {
           <span>as ${element.character}</span>
         </div>
         </li>`
-      }).join('');
+      }).slice(0, 10).join('');
 
-      //console.log(movieCast);
-      console.log(element.title)
-      const pageTemplate = `
-        
+      const moviePageTemplate = `
         <div class="movie-info">
           <div class="movie-info-wrapper">
             <div class="movie-overview-wrapper">
@@ -165,11 +163,10 @@ function showPopup(element) {
             </div>
               <div class="movie-overview">
                 <p>${data.overview}</p>
-                ${homePage}
+                ${movieHomePage}
               </div>
-              ${trailer}
+              ${movieTrailer}
             </div>  
-              
             <div class="movie-actors">
               <h3>Top cast</h3>
               <ul class="actors-list">
@@ -177,35 +174,36 @@ function showPopup(element) {
               </ul>
             </div>
           </div>
-        </div>`
-        
-      moviePage.insertAdjacentHTML('beforeend', pageTemplate);
-    });
-  });
+        </div>` 
+        moviePage.insertAdjacentHTML('beforeend', moviePageTemplate);
 
+        const genresBtns = document.querySelector('.movie-genres');
+        genresBtns.addEventListener('click', getGenreData);
+
+        async function getGenreData(event) {
+          let idCollection = event.target.getAttribute('id');
+          const res = await fetch(baseUrl+'/discover/movie/?'+apiKey+'&with_genres='+idCollection);
+          const data = await res.json();
+          moviePage.innerHTML = '';
+          closePopup();
+          showData(data.results)
+          window.scrollTo({
+            top: 600,
+            behavior: "smooth"
+          });
+        }
+
+      });
+  });
   document.querySelector('.overlay').style.display = "block";
   document.body.classList.toggle('overlay-open');
 }
 
-
 document.querySelector('.overlay-close').addEventListener('click', closePopup);
-
-function closePopup () {
+function closePopup (){
   document.querySelector('.overlay').style.display = "none";
   moviePage.innerHTML = '';
   document.body.classList.toggle('overlay-open');
-}
-
-function previewData(data) {
-  const card = document.querySelectorAll('.card');
-  card.forEach(item => {
-    item.addEventListener('click', (event) => {
-      console.log(`${event.target} click`);
-      let currentId = event.target.getAttribute('data-id');
-      console.log(currentId);
-      previewData(`http://api.themoviedb.org/3/movie/${currentId}?api_key=1939abe3d00976407f86acd63c341f94&append_to_response=videos`);
-    });
-  });
 }
 
 window.onload = function() {
@@ -215,32 +213,24 @@ window.onload = function() {
 form.addEventListener('submit', (event) =>{
   event.preventDefault();
   const searchQuery = search.value;
-
   if (searchQuery) {
     getData(searchUrl+'&query='+searchQuery);
     search.value = searchQuery;
   } else {
     getData(url);
   }
-})
+});
 
+search.addEventListener("input", function(){ 
+  if (search.value.length > 0) {
+    clearFormBtn.classList.add('show');
+  } else {
+    clearFormBtn.classList.remove('show');
+  }
+});
 
-
-
-
-  //const videoContainer = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${video}"></iframe>`
-
-
-  //moviesList.insertAdjacentHTML('beforeend', videoContainer);
-/*
-  const imgContainer = `<img class="card__img" src="https://image.tmdb.org/t/p/w500/${img}" alt="${title}">`;
-  const titleContainer = `<div class="card__info"><h3 class="card__title">${title}</h3>`;
-  const ratingContainer = `<span class="card__rating">${rating}</span>`
-  const overviewContainer = `<div class="card__overview">${overview}</div>`
-  */
-
-/*
-<iframe width="560" height="315"
-src="https://www.youtube.com/embed/KlyknsTJk0w">
-</iframe>
-*/
+clearFormBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  search.value = '';
+  clearFormBtn.classList.remove('show');
+});
